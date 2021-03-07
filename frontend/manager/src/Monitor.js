2a23c1420de01da778807578b56b2d5cce5a7557
tsusa <timmsusa@googlemail.com>
2021-03-07 16:52:10 +0100

    feat: Beautify MIDI Input monitor

--- frontend/manager/src/Monitor.js
+++ frontend/manager/src/Monitor.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import MonitorTable from './MonitorTable.js'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,22 +19,15 @@ export function Monitor(props) {
 
 
   useEffect(() => {
-    const tmp = [...value, '\n', text, '\n']
-    setValue(tmp)
+    const { in: inpT, channel, note, velocity, _type } = JSON.parse(text) || {}
+    const tmp = value.concat({ input: inpT, channel, note, velocity, _type })
+    const tpm = tmp.filter((item) => !!item.input)
+    setValue(tpm)
   }, [text])
-
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField
-        style={{ width: '80vw' }}
-        id="outlined-multiline-static"
-        label="MIDI"
-        multiline
-        rows={16}
-        defaultValue="Monitor"
-        variant="outlined"
-        value={value}
-      />
-    </form>
+    <React.Fragment>
+      <MonitorTable rows={value}></MonitorTable>
+    </React.Fragment>
+
   );
 }
